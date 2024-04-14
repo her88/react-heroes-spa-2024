cbc684f773a0e5cad155630e4543b39d36e35124
hernanperez <hernan.perez@almundo.com>
2024-04-14 16:24:43 -0300

    195. Lista de Heroes

--- src/heroes/pages/DcPage.jsx
+++ src/heroes/pages/DcPage.jsx
@@ -1,6 +1,15 @@
+import { HeroList } from "../components";
 
 export const DcPage = () => {
+
+  const publisher = 'DC Comics';
+
   return (
-    <h1>DcPage</h1>
+    <>
+      <h1>DC Comics</h1>
+      <hr />
+
+      <HeroList publisher={publisher} />
+    </>
   )
 }
