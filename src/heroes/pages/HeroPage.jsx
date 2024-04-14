637ef763b5a2cdf62d2a76ef52fc1d41eefaed9a
hernanperez <hernan.perez@almundo.com>
2024-04-14 19:33:45 -0300

    201. Animaciones en nuestro componente

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -30,7 +30,7 @@ export const HeroPage = () => {
                 <img
                     src={`/assets/heroes/${hero.id}.jpg`}
                     alt={hero.superhero}
-                    className="img-thumbnail" 
+                    className="img-thumbnail animate__animated animate__fadeInLeft" 
                 />
             </div>
 
