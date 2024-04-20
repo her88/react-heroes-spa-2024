c943daaa0daa226f902bbf48ef453c25e71ba738
hernanperez <hernan.perez@almundo.com>
2024-04-20 15:27:35 -0300

    214. Logout del usuario

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -32,10 +32,18 @@ export const AuthProvider = ({ children }) => {
         dispatch(action);
     }
 
+    const logout = () => {
+        const action = { type: types.logout };
+        localStorage.removeItem('user');
+        dispatch(action);
+    }
+
     return (
         <AuthContext.Provider value={{
             ...authState,
-            login: login
+            //Methods
+            login,
+            logout
         }}>
             {children}
         </AuthContext.Provider>
